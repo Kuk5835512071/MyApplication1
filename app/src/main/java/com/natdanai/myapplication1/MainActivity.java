1f969bafccda9b9e3b26258a89cb4a3c42dfecf0
Natdanai <5835512071@psu.ac.th>
2018-02-09 11:19:11 +0700

    new project

--- app/src/main/java/com/natdanai/myapplication1/MainActivity.java
+++ app/src/main/java/com/natdanai/myapplication1/MainActivity.java
@@ -3,6 +3,7 @@ package com.natdanai.myapplication1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -11,14 +12,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv1;
     private TextView tv2;
     private EditText edtName;
+    private Button btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edtName = findViewById(R.id.edtName);
+        btnDelete = findViewById(R.id.btnDelete);
         tv1 = findViewById(R.id.tv1);
         tv2 = findViewById(R.id.tv2);
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv1.setText("");
+                tv2.setText("");
+            }
+        });
     }
 
     public void submit(View view) {
